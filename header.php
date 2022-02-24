ab811121113dfeafd91ac232ae45d8cc98c7c100
Alex <Yukako22@live.fr>
2022-02-24 15:28:30 -0500

    Ajout de barre de recherche

--- header.php
+++ header.php
@@ -20,11 +20,17 @@
             <?php bloginfo('name'); ?></h1>
         </a>
     <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+        <form action="<?php echo get_home_url('/') ?>" class="recherche" method="get" >
+        <input type="text" class="recherche__input" name="s" value="<?php echo get_search_query(); ?>">
+        <button class="recherche__bouton">
+            <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#0326b5"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </button>
+    </form>
 </header>
 <section class="site__barre">
     <input type="checkbox"  id="chk-burger">
     <label for="chk-burger" id="burger">
-        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ddd"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#D2691E"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </label>
     <?php wp_nav_menu(array("menu"=>"principal",
                             "container"=>"nav")); ?>
---
+++ search.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<main class="site__main">
+    <?php if(have_posts()) ?>
+
+</main>
+<?php get_footer(); ?>
