6bbd7cf97c234a70d8a2d0f24cdbeb9d98e4e90b
Alex <Yukako22@live.fr>
2022-02-24 14:50:02 -0500

    Nyaa

--- 404.php
+++ 404.php
@@ -1,7 +1,14 @@
 <?php get_header() ?>
 <main class="site__main">
 
-    <h1>---- Erreur 404------</h1>
+    <h1>---- Erreur 404 ------</h1>
+
+    <section class="erreur-404">
+
+    <h1>
+         La page n'existe pas
+    </h1>
+    </section>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>        
