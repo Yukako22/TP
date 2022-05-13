bddd6dee2057d2c241c4cd6a6fb74d27b581db37
Alexandre Boudreau <e2006176@cmaisonneuve.qc.ca>
2022-05-13 19:26:07 -0400

    class sur les img du carrousel + design

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
         </div>
         <?php get_search_form(); ?>
     </section>
-        
+    
 </header>
 <section class="site__barre">
     <input type="checkbox"  id="chk-burger">
@@ -68,3 +68,6 @@
     <?php wp_nav_menu(array("menu"=>"principal",
                             "container"=>"nav")); ?>
 </section>
+
+
+    
